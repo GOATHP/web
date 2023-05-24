56155a862fd619eb27794f808d8d1bdfa68a9ef8
Mongmwa <63557303+Mongmwa@users.noreply.github.com>
2023-05-24 10:56:56 +0900

    fix readme

--- src/let01.js
+++ src/let01.js
@@ -6,21 +6,13 @@ result = x + y--;
 console.log(result)
 console.log(y)
 console.log('----')
-result = x + y-- +5;
+result = x + y-- + 5;
 console.log(result)
 console.log(y)
 console.log('----')
 result = ++x - y;
 console.log(result)
 console.log(y)
-
-x = 5 
-y = 5
-y += x
-y -= x
-y *= x
-y /= x
-y %= x
 
 y += x;
 console.log(y);
@@ -36,3 +28,25 @@ console.log(y);
 
 y %= x;
 console.log(y);
+
+
+function birthCheck() {
+    const currentYear = 2023;
+    let birthYear;
+    let age;
+
+    birthYear = parseInt(prompt("태어난 연도"));
+    if (isNaN(birthYear)) {
+        alert(`태어난 연도를 입력하세요.`);
+        return;
+    } else {
+        age = currentYear - birthYear + 1;
+        if (age < 20) {
+            alert("성인이 아닙니다.");
+        }
+        alert(`${currentYear}년 현재 당신은 ${age}살입니다.`);
+    }
+}
+
+
+birthCheck();
