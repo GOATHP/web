b4e5ffa2faf0dc611bb14c92f61a65c1b565b7a0
Mongmwa <63557303+Mongmwa@users.noreply.github.com>
2023-05-24 14:45:44 +0900

    fix readme

--- src/if04.js
+++ src/if04.js
@@ -1,6 +1,17 @@
-let userNumber = prompt("숫자를 입력하세요");
+// let userNumber = prompt("숫자를 입력하세요");
 
-if (userNumber !== null) {
-    userNumber = parseInt(userNumber);
-    (userNumber % 2) ? alert(`${userNumber}: 홀수`) : alert(`${userNumber}:짝수`)
+// if (userNumber !== null) {
+//     userNumber = parseInt(userNumber);
+//     (userNumber % 2) ? alert(`${userNumber}: 홀수`) : alert(`${userNumber}:짝수`)
+// }
+
+let name="hello";
+
+switch(name) {
+    case 'hello':
+        console.log("hello world");
+        break;
+    case 'park':
+        console.log("박태현 화이팅");
+        break;
 }
